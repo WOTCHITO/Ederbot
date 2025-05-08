61bef0e0b1de2149015f84df0accbcb1ece396be
E D E R <139829285+WOTCHITO@users.noreply.github.com>
2025-05-08 17:16:00 -0500

    Update _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -3,11 +3,15 @@ import fetch from 'node-fetch'
 
 export async function before(m, { conn, participants, groupMetadata }) {
   if (!m.messageStubType || !m.isGroup) return true
-
   let who = m.messageStubParameters[0]
   let taguser = `@${who.split('@')[0]}`
   let chat = global.db.data.chats[m.chat]
-  let defaultImage = 'https://files.catbox.moe/xr2m6u.jpg';
+  let defaultImage = 'https:                               
+
+  if (chat.welcome) {
+    let img;
+    try {
+      let pp = await conn.profilePictureUrl(who, '//files.catbox.moe/xr2m6u.jpg';
 
   if (chat.welcome) {
     let img;
@@ -19,13 +23,9 @@ export async function before(m, { conn, participants, groupMetadata }) {
     }
 
     if (m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_ADD) {
-      let bienvenida = `🌱 *Bienvenido* a ${groupMetadata.subject}\n ${taguser}\n  SIGUE MI CANAL DONDE SUBO MODS TODO LOS DIAS🥰✨ https://whatsapp.com/channel/0029VaFMguNDTkJzaPGXAG3D!`
+      let bienvenida = `🌱 *Bienvenido* a ${groupMetadata.subject}\n ${taguser}\n SIGUE MI CANAL DONDE SUBO MODS TODO LOS DIAS🥰✨ https://whatsapp.com/channel/0029VaFMguNDTkJzaPGXAG3D!`
       await conn.sendMessage(m.chat, { image: img, caption: bienvenida, mentions: [who] })
-    } else if (m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_REMOVE || m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_LEAVE) {
-      let bye = `💮 *Adiós* De ${groupMetadata.subject}\n ${taguser}\n Te esperamos pronto!`
-      await conn.sendMessage(m.chat, { image: img, caption: bye, mentions: [who] })
     }
   }
-
   return true
-}
+    }
